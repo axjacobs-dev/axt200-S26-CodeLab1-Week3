511a0aa85be1dbe511d72c4ca267e3ef1788408c
axjacobs-dev <axjacobs@gmail.com>
2026-02-11 23:37:09 -0500

    updated scripts
    adding scoring property
    added stranger prefab
    added destroy on collision script attached to stranger object
    added score text to Canvas

--- CodeLab1-Week3/Assets/Scripts/ObjectPermanence.cs
+++ CodeLab1-Week3/Assets/Scripts/ObjectPermanence.cs
@@ -1,14 +1,40 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class ObjectPermanence : MonoBehaviour
 {
     public static ObjectPermanence instance;
     public int currentLevel = 0;
+    public TMP_Text scoreText;
+    private string defaultScoreText = "Interactions: <score>";
+    //using this to helps avoid errors due to typos
+    const string KeyScore = "Score";
+    //variable for current score
+    private int score;
+    //the property that will modify the variable score
+    public int Score
+    {
+        set
+        {   
+            Debug.Log("You have interacted with:" + value +" strangers");
+            //I'm going to use playerprefs
+            score = value;
+            PlayerPrefs.SetInt(KeyScore, score);
+        }
+        get
+        {
+            score = PlayerPrefs.GetInt(KeyScore);
+            Debug.Log("You have interacted with a total of:" + score + " strangers");
+            return score;
+        }
+    }
+    
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        Score = 0;
         //checks to see if the instance exists
         if (instance == null)
         {
@@ -23,8 +49,21 @@ public class ObjectPermanence : MonoBehaviour
             Debug.Log("There can be only one.");
             Destroy(gameObject);
         }
+    }
 
+    void Update()
+    {
+        string updatedScoreText = defaultScoreText;
 
+        // Replace placeholders with current values
+        updatedScoreText = updatedScoreText.Replace("<score>", Score + ""); //always use the property to make sure the value is properly loaded
+        //updatedScoreText = updatedScoreText.Replace("<high>", HighScore + "");
+
+        // Update the UI text if assigned (avoid null reference)
+        if (scoreText != null)
+        {
+            scoreText.text = updatedScoreText;
+        }
     }
 
     // Update is called once per frame
