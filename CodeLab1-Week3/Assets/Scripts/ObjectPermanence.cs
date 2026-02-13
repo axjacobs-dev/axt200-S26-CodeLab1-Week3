17117b04cc540497e4149ce7ccef80cfc2db5588
axjacobs-dev <axjacobs@gmail.com>
2026-02-13 09:51:57 -0500

    updated script

--- CodeLab1-Week3/Assets/Scripts/ObjectPermanence.cs
+++ CodeLab1-Week3/Assets/Scripts/ObjectPermanence.cs
@@ -4,29 +4,39 @@ using TMPro;
 
 public class ObjectPermanence : MonoBehaviour
 {
+    //static means it doesn't change for all instances of it
     public static ObjectPermanence instance;
+    //goes by numbered order in the build rather than scene name
     public int currentLevel = 0;
     public TMP_Text scoreText;
     private string defaultScoreText = "Interactions: <score>";
-    //using this to helps avoid errors due to typos
+    private string updatedScoreText;
+    //constant means the variable type will always be the same
+    //averts problems due to typos
+    //KeyScore saves data to PlayerPrefs
     const string KeyScore = "Score";
     //variable for current score
     private int score;
-    //the property that will modify the variable score
+    //the property that will modify the score variable
     public int Score
     {
-        set
-        {   
-            Debug.Log("You have interacted with " + value +" strangers");
-            //I'm going to use PlayerPrefs
-            score = value;
-            PlayerPrefs.SetInt(KeyScore, score);
-        }
-        get
+        get //reading
         {
-            score = PlayerPrefs.GetInt(KeyScore);
+            //retrieves score from PlayerPrefs
+            score = PlayerPrefs.GetInt(KeyScore); 
             Debug.Log("You have interacted with a total of " + score + " strangers");
+            //returns value of score variable
             return score;
+        }
+        set  //writing
+        {   
+            
+            //I'm going to use PlayerPrefs
+            //sets score variable to value of property Score
+            score = value;
+            Debug.Log("You have interacted with " + value +" strangers");
+            PlayerPrefs.SetInt(KeyScore, score);
+            //can add juice here
         }
     }
     
@@ -34,30 +44,35 @@ public class ObjectPermanence : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        //starting with a Score value of zero
         Score = 0;
+        //call the singleton at Start
+        //this script bundles the player and game manager as one singleton
+        //this may be a mistake
         //checks to see if the instance exists
         if (instance == null)
         {
-            //if instance does not exist this line sets it
+            //if instance of THIS class does not exist this line sets it
             instance = this;
             //object will not be destroyed in next scene
             DontDestroyOnLoad(gameObject);
         }
         else
         {
-            //but if another instance exists this one gets destroyed
+            //but if another instance of this class exists this one gets destroyed
             Debug.Log("There can be only one.");
             Destroy(gameObject);
         }
     }
-
+    // Update is called once per frame
+    //this score is constantly updating in the console
+    //so will move this to the property and create an update score function
     void Update()
     {
-        string updatedScoreText = defaultScoreText;
-
-        // Replace placeholders with current values
-        updatedScoreText = updatedScoreText.Replace("<score>", Score + ""); //always use the property to make sure the value is properly loaded
-        //updatedScoreText = updatedScoreText.Replace("<high>", HighScore + "");
+        updatedScoreText = defaultScoreText;
+        // Replace placeholders with current values 
+        updatedScoreText = updatedScoreText.Replace("<score>", Score.ToString()); 
+        //always use the property to make sure the value is properly loaded
 
         // Update the UI text if assigned (avoid null reference)
         if (scoreText != null)
@@ -65,13 +80,13 @@ public class ObjectPermanence : MonoBehaviour
             scoreText.text = updatedScoreText;
         }
     }
-
-    // Update is called once per frame
+    //function for changing scenes
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Exit"))
         {
             Debug.Log("Scene Change has been triggered.");
+            //goes to the next level by number order in build
             currentLevel++;
             SceneManager.LoadScene(currentLevel);
             
